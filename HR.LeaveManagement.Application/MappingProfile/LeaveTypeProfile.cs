7f5436a9589bfb7f9454eea8e86ab2521f6aca11
apalan60 <apalan60@gmail.com>
2023-07-26 16:27:53 +0800

    add LeaveTypeProfile implement Profile (Automapper)

---
+++ HR.LeaveManagement.Application/MappingProfile/LeaveTypeProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfile;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+    }
+}
