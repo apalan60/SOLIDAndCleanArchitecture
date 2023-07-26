372e19d8ecfe68ec5c278cf700dcfa2fe19783b7
apalan60 <apalan60@gmail.com>
2023-07-27 06:44:30 +0800

    Delete Command

---
+++ HR.LeaveManagement.Application/Features/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)   
+    =>   this._leaveTypeRepository = leaveTypeRepository;
+    
+
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        //get entity by id
+        LeaveType leaveType = await _leaveTypeRepository.GetByIdAsync(request.Id);
+        // verify
+
+        //void delete 
+        await _leaveTypeRepository.DeleteAsync(leaveType);
+        return Unit.Value;
+    }
+}
