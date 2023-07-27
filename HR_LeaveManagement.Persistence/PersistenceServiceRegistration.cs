400b8117c75c666bd4b58437c19a25de5b8b4152
apalan60 <apalan60@gmail.com>
2023-07-27 19:58:49 +0800

    start implementing Infrastructure Layer add HR_LeaveManagement.Persistence
    Nugget:
    SQL Server
    Microsoft.Extensions.Options.ConfigurationExtension
    
    add PersistenceServiceRegistration

---
+++ HR_LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HR_LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services) 
+    {
+        return services;
+    }
+}
