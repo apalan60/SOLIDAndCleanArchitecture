878b69534c5a6e257fea16d2035942c1f3d7e41f
apalan60 <apalan60@gmail.com>
2023-07-28 11:33:35 +0800

    implement LeaveRequestRepository

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -4,5 +4,7 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence;
 
 public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
 {
-
+    Task<LeaveRequest?> GetLeaveRequestWithDetails(int id);  //query by included method  
+    Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
+    Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId);
 }
--- HR_LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HR_LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,8 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
 using HR_LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace HR_LeaveManagement.Persistence.Repositories;
 
@@ -8,5 +10,33 @@ public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveReq
 {
     public LeaveRequestRepository(HrDatabaseContext context) : base(context)
     {
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+    {
+        List<LeaveRequest>? leaveRequest = await _context.LeaveRequests
+            .Include(request => request.LeaveType)
+            .ToListAsync();
+
+        return leaveRequest;
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+    {
+        List<LeaveRequest> leaveRequest = await _context.LeaveRequests
+            .Where(request => request.RequestingEmployeId == userId)
+            .Include(request => request.LeaveType)
+            .ToListAsync();
+
+        return leaveRequest;
+    }
+
+    public async Task<LeaveRequest?> GetLeaveRequestWithDetails(int id)
+    {
+        LeaveRequest? leaveRequest = await _context.LeaveRequests
+            .Include(request => request.LeaveType)
+            .FirstOrDefaultAsync(request => request.Id == id);
+
+        return leaveRequest;
     }
 }
