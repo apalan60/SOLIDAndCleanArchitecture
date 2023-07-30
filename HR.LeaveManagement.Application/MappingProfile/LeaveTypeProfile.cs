54d18621609bc3abf50e701c18db762be4288e87
apalan60 <apalan60@gmail.com>
2023-07-30 16:41:54 +0800

    API CRUD and Debug Validator、AutoMapper

--- HR.LeaveManagement.Application/MappingProfile/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Application/MappingProfile/LeaveTypeProfile.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveTypeFeature.Commands.CreateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypeFeature.Commands.UpdateLeaveType;
 using HR.LeaveManagement.Application.Features.LeaveTypeFeature.Queries.GetAllLeaveTypes;
 using HR.LeaveManagement.Application.Features.LeaveTypeFeature.Queries.GetLeaveTypeDetail;
 using HR.LeaveManagement.Domain;
@@ -15,7 +17,11 @@ public class LeaveTypeProfile : Profile
     public LeaveTypeProfile()
     {
         CreateMap<LeaveTypeDto, LeaveType>().ReverseMap(); //類別有需要往回轉型時添加，此處為示範
-
+        
         CreateMap<LeaveType, LeaveTypeDetailsDto>();
+        
+        CreateMap<CreateLeaveTypeCommand, LeaveType>();
+
+        CreateMap<UpdateLeaveTypeCommand, LeaveType>();
     }
 }
