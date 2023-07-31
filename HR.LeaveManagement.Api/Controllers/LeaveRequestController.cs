a936537e6e03715bbe50ff44d6cf6277af01da9d
apalan60 <apalan60@gmail.com>
2023-07-31 15:23:17 +0800

    Add LeaveRequestController

---
+++ HR.LeaveManagement.Api/Controllers/LeaveRequestController.cs
@@ -0,0 +1,105 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.ChangeLeaveRequestApproval;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+using HR.LeaveManagement.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveRequestController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveRequestController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        // GET: api/<LeaveRequestController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveRequest>>> Get()
+        {
+            var leaveRequests = await _mediator.Send(new GetLeaveRequsetQuery());
+            return Ok(leaveRequests);
+        }
+
+        // GET api/<LeaveRequestController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveRequest>> Get(int id)
+        {
+            var leaveRequests = await _mediator.Send(new GetLeaveRequestDetailQuery(id));
+            return Ok(leaveRequests);
+        }
+
+        // POST api/<LeaveRequestController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Post(CreateLeaveRequestCommand leaveRequest)
+        {
+            var response = await _mediator.Send(leaveRequest);
+            return CreatedAtAction(nameof(Get), new { Id = response });
+        }
+
+        // PUT api/<LeaveRequestController>/5
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Put(UpdateLeaveRequestCommand leaveRequest)
+        {
+            await _mediator.Send(leaveRequest);
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("CancelLeaveRequest")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> CancelLeaveRequest(CancelLeaveRequestCommand cancelLeaveRequest)
+        {
+            await _mediator.Send(cancelLeaveRequest);
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("UpdateApproval")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateApproval(ChangeLeaveRequestApprovalCommand updateApprovalRequest)
+        {
+            await _mediator.Send(updateApprovalRequest);
+            return NoContent();
+        }
+
+
+        // DELETE api/<LeaveRequestController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteLeaveAllocationCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/ChangeLeaveRequestApproval/ChangeLeaveRequestApprovalCommand.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Email;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.ChangeLeaveRequestApproval;
+
+public class ChangeLeaveRequestApprovalCommandHandler : IRequestHandler<ChangeLeaveRequestApprovalCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly IEmailSender _emailSender;
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public ChangeLeaveRequestApprovalCommandHandler(
+        IMapper mapper,
+        IEmailSender emailSender,
+        ILeaveRequestRepository leaveRequestRepository,
+        ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._mapper = mapper;
+        this._emailSender = emailSender;
+        this._leaveRequestRepository = leaveRequestRepository;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+
+
+    public async Task<Unit> Handle(ChangeLeaveRequestApprovalCommand request, CancellationToken cancellationToken)
+    {
+        var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+        await UpdateLeaveRequestApprovalStatus(request, leaveRequest);
+        await SendConfirmEmail(leaveRequest);
+        return Unit.Value;
+    }
+
+    private async Task SendConfirmEmail(Domain.LeaveRequest? leaveRequest)
+    {
+        try
+        {
+            var email = new EmailMessage
+            {
+                To = string.Empty, /* Get email from employee record */
+                Body = $"The approval status for your leave request for {leaveRequest!.StartDate:D} to {leaveRequest.EndDate:D} has been updated.",
+                Subject = "Leave Request Approval Status Updated"
+            };
+            await _emailSender.SendEmail(email);
+        }
+        catch (Exception)
+        {
+            // log error
+        }
+    }
+
+    private async Task UpdateLeaveRequestApprovalStatus(ChangeLeaveRequestApprovalCommand request, Domain.LeaveRequest? leaveRequest)
+    {
+        if (leaveRequest == null)
+        {
+            throw new NotFoundException(nameof(request), request.Id);
+        }
+        leaveRequest.Approved = true;
+        await _leaveRequestRepository.UpdateAsync(leaveRequest);
+    }
+}
