3823cc573fabba3d71797d936a946ac196beb497
apalan60 <apalan60@gmail.com>
2023-09-17 21:03:39 +0800

    Complete IntergrationTests

--- HR.LeaveManagement.Persistence.IntergrationTests/HrDatabaseContextTests.cs
+++ HR.LeaveManagement.Persistence.IntergrationTests/HrDatabaseContextTests.cs
@@ -1,5 +1,7 @@
+using HR.LeaveManagement.Domain;
 using HR_LeaveManagement.Persistence.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
+using Shouldly;
 
 namespace HR.LeaveManagement.Persistence.IntergrationTests;
 
@@ -18,10 +20,37 @@ public class HrDatabaseContextTests
     public async void Save_SetDateCreatedValue()
     {
         //Arrange
-
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
 
         //Act
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+        await _hrDatabaseContext.SaveChangesAsync();
 
         //Assert
+        leaveType.DateCreated.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async void Save_SetDateModifiedValue()
+    {
+        //Arrange
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
+
+        //Act
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+        await _hrDatabaseContext.SaveChangesAsync();
+
+        //Assert
+        leaveType.DateModified.ShouldNotBeNull();
     }
 }
